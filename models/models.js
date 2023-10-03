80f807c72b9f1df0b375a568b32888597d087d59
Yve Volkovaya <yvevolkovaya@gmail.com>
2023-10-03 11:22:38 +0100

    add get /api/articles endpoint

--- models/models.js
+++ models/models.js
@@ -8,13 +8,19 @@ exports.fetchTopics = () => {
 
 exports.fetchArticleById = (article_id) => {
    return db.query("SELECT * FROM articles WHERE article_id=$1;", [article_id])
-   .then(({rows}) => {
-      return rows;
+   .then((result) => {
+      if (result.rows.length === 0){
+         return Promise.reject({
+            status: 404,
+            message: 'invalid article id'
+         })
+      }
+      return result.rows;
    })
 }
 
 exports.fetchArticles = () => {
-   return db.query(`SELECT * FROM articles;`).then(({rows}) => {
+   return db.query(`SELECT article_id, title, topic, author, created_at, votes, article_img_url FROM articles ORDER BY created_at DESC;`).then(({rows}) => {
       return rows
    })
 }
